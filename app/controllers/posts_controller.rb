4f2dbcb07989b3c2a22d129c2e6c83eaeca99c64
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-17 15:10:34 -0400

    finished rails form tag readme lab

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,12 @@
 class PostsController < ApplicationController
+  def index
+  end
 
+  def new
+  end
+
+  def create
+    @data = {title: params[:title], desc: params[:description]}
+    redirect_to new_post_path(post: @data)
+  end
 end
